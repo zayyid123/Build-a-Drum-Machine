8027a768a06cde72f4c02c89c58efbbb985879f5
zayyid123 <mochamadzayyid@gmail.com>
2022-05-14 11:30:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {audiosBank} from './constants'
+import {audios} from './constants'
 
 function App() {
+  const [power, setPower] = React.useState(true)
+  const [bank, setBank] = React.useState(false)
+  const [dataBtn, setDataBtn] = React.useState(bank ? audiosBank : audios)
+
+  //keep the same value with UI display
+  const [vol, setVol] = React.useState(0.5)
+
+  const regClick = (event, id) => {
+    console.log(id)
+    var display = document.getElementById('display');
+    display.innerHTML = id
+    var audio = document.getElementById(event);
+    audio.play();
+  }
+
+  const bankClick = (event, id) => {
+    console.log(id)
+    var display = document.getElementById('display');
+    display.innerHTML = id
+    var audio = document.getElementById(event);
+    audio.play();
+  }
+
+  const handleClick = (event)=> {
+      if(power){
+          if(power && bank){
+              bankClick(event.target.value, event.target.id);
+          } else if (power && !bank) {
+              regClick(event.target.value, event.target.id);
+          }
+      } else if((!power && !bank) || (!power && bank)) {
+          alert('Hi, power is off! Please turn on :)')
+      }
+  }
+
+  window.document.onkeyup = function(event) {
+    let id = ''
+    let keystroke = event.key.toUpperCase()
+        if (keystroke === "q" || keystroke === "Q") {
+          id = dataBtn[0].myID
+        } else if (keystroke === "w" || keystroke === "W") {
+          id = dataBtn[1].myID 
+        } else if (keystroke === "e" || keystroke === "E") {
+          id = dataBtn[2].myID 
+        } else if (keystroke === "a" || keystroke === "A") {
+          id = dataBtn[3].myID 
+        } else if (keystroke === "s" || keystroke === "S") {
+          id = dataBtn[4].myID 
+        } else if (keystroke === "d" || keystroke === "D") {
+          id = dataBtn[5].myID 
+        } else if (keystroke === "z" || keystroke === "Z") {
+          id = dataBtn[6].myID 
+        } else if (keystroke === "x" || keystroke === "X") {
+          id = dataBtn[7].myID 
+        } else if (keystroke === "c" || keystroke === "C") {
+          id = dataBtn[8].myID 
+        }
+    if(power){
+      if(power && bank){
+          bankClick(keystroke, id);
+      } else if (power && !bank) {
+          regClick(keystroke,id);
+      }
+  } else if((!power && !bank) || (!power && bank)) {
+      alert('Hi, power is off! Please turn on :)')
+  }
+  }
+
+  function switchPower() {
+    setPower(!power);
+  }
+
+  function switchBank() {
+    setBank(!bank);
+  }
+
+  React.useEffect(() => {
+    setDataBtn(bank ? audiosBank : audios)
+  }, [bank])
+  
+
+function sliderCallback(data){
+    setVol(data);
+    console.log('Received volume is ' + data);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div  id="drum-machine" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexWrap:'wrap', backgroundColor:'#161B22', width: 450,marginTop: 50,borderRadius: 10, margin:'auto'}}>
+        <div style={{padding: 10, display: 'flex', flexWrap:'wrap', width: 170}}>
+          {
+            dataBtn.map((item, index) => 
+            <button type="submit" style={{margin: 2, minWidth: 50, minHeight: 50}} key={index} id={item.myID} className='drum-pad' value={item.name} onClick={handleClick}>
+              <audio className='clip' id={item.name} src={item.url}></audio>
+              {item.name}
+            </button>)
+          }
+        </div>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <button onClick={switchPower} style={{margin: 5, width: 60}}>{power ? 'On' : 'Off'}</button>
+          <button onClick={switchBank} style={{margin: 5, width: 60}}>{bank ? 'On' : 'Off'}</button>
+          <div id="display" style={{backgroundColor: 'white', width: 110, padding: 5, margin: 5}}>-</div>
+        </div>     
+      </div>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,104 @@
+import Q from './assets/Heater-1.mp3'
+import W from './assets/Heater-2.mp3'
+import E from './assets/Heater-3.mp3'
+import A from './assets/Heater-4_1.mp3'
+import S from './assets/Heater-6.mp3'
+import D from './assets/Dsc_Oh.mp3'
+import Z from './assets/Kick_n_Hat.mp3'
+import X from './assets/RP4_KICK_1.mp3'
+import C from './assets/Cev_H2.mp3'
+
+export const audios = [
+    {
+        name: 'Q',
+        myID: 'Heater-1',
+        url: Q
+    },
+    {
+        name: 'W',
+        myID: 'Heater-2',
+        url: W
+    },
+    {
+        name: 'E',
+        myID: 'Heater-3',
+        url: E
+    },
+    {
+        name: 'A',
+        myID: 'Heater-4_1',
+        url: A
+    },
+    {
+        name: 'S',
+        myID: 'Heater-6',
+        url: S
+    },
+    {
+        name: 'D',
+        myID: 'Dsc_Oh',
+        url: D
+    },
+    {
+        name: 'Z',
+        myID: 'Kick_n_Hat',
+        url: Z
+    },
+    {
+        name: 'X',
+        myID: 'RP4_KICK_1',
+        url: X
+    },
+    {
+        name: 'C',
+        myID: 'Cev_H2',
+        url: C
+    }
+];
+export const audiosBank = [
+    {
+        name: 'Q',
+        myID: 'Chord_1',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    },
+    {
+        name: 'W',
+        myID: 'Chord_2',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    },
+    {
+        name: 'E',
+        myID: 'Chord_3',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    },
+    {
+        name: 'A',
+        myID: 'Give_us_a_light',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    },
+    {
+        name: 'S',
+        myID: 'Dry_Ohh',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+    },
+    {
+        name: 'D',
+        myID: 'Bld_H1',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+    },
+    {
+        name: 'Z',
+        myID: 'punchy_kick_1',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+    },
+    {
+        name: 'X',
+        myID: 'side_stick_1',
+        url:'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+    },
+    {
+        name: 'C',
+        myID: 'Brk_Snr',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+    }
+];
